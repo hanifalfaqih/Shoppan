b8e151ab39bcdcfffce328722a5fcd35ffcc82c3
rifkiaditya <rifkiaditya24545@gmail.com>
2023-11-02 22:32:09 +0700

    SLicing layout detailProduct

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,5 +66,7 @@ dependencies {
     implementation("com.intuit.sdp:sdp-android:1.1.0")
     implementation("com.intuit.ssp:ssp-android:1.1.0")
 
+    // Glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 
 }
